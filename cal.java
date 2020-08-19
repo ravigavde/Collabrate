a0b3b38f70ed53c61115fcfec9fbdfe4d4619e18
ravigavde <nightr714@gmail.com>
2020-08-19 14:52:45 +0530

    added sub method in sub  branch

---
+++ cal.java
@@ -0,0 +1,13 @@
+class cal
+{
+    int sub(int a , int b)
+    {
+        return(a-b);
+    }
+    public static void main(String arg[])
+    {
+        cal c = new cal();
+        int a=9, int b =9;
+        System.out.println("Subtraction is "+c.sub(a,b));
+    }
+}
