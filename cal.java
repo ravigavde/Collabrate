38e7581aa520ce6df2091882be10eda902531fac
Manasi <Manasi@cue>
2020-08-19 14:34:02 +0530

    initialize add method

--- cal.java
+++ cal.java
@@ -2,6 +2,9 @@ class cal
 {
     public static void main(String arg[])
     {
-        
+	    int num1=10;
+	    int num2=15;
+	    int sum = num1 + num2;
+	    system.out.println("Sum = "+sum);
     }
 }
