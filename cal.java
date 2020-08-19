0403823fdd07809099b112f5e686736e5dd49cda
Manasi <Manasi@cue>
2020-08-19 15:08:49 +0530

    add div method

---
+++ cal.java
@@ -0,0 +1,14 @@
+class cal
+{
+	float div(float a,float b)
+	{
+		return(a/b);
+	}
+	public static void main(String arg[])
+	{
+		cal c = new cal();
+		float x=12,y=6;
+		System.out.println("Division is : "+c.div(x,y))
+	}
+}
+
